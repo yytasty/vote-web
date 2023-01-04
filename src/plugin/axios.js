6241168aa696cbafc4584b3fb96b65a1ba8a72ee
yy <651041159@qq.com>
2023-01-04 13:53:29 +0800

    feat: 添加新服务器地址

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 // const baseURL = '//api.business.tanchi0757.shop'
 // const baseURL = 'http://8.134.143.87:9027/';
-const baseURL = 'http://www.yytasty.com:9027/';
+// const baseURL = 'http://www.yytasty.com:9027/';
+const baseURL = 'http://8.134.123.115:9027/';
 // const baseURL = 'http://localhost:26000/api'
 
 let config = {
